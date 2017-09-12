b8bf3b4e6ddb0923acd0b7ccebf717a262d484bc
elgood <eric.goodman@colorado.edu>
2017-09-12 08:21:11 -0600

    Checking in latest.

--- scripts/learning.py
+++ scripts/learning.py
@@ -11,12 +11,11 @@ import sklearn
 import matplotlib.pyplot as plt
 import pandas
 import math
+import operator
 
 def find_optimal_cutoff_closest_to_perfect(fpr, tpr, threshold):
   distance = float("inf")
   threshold_index = -1
-  print(type(fpr))
-  print(fpr)
   for i in range(len(fpr)):
     d1 = math.sqrt((fpr[i] - 0) * (fpr[i] - 0) + (tpr[i] - 1) * (tpr[i] - 1))
     if d1 < distance:
@@ -25,7 +24,21 @@ def find_optimal_cutoff_closest_to_perfect(fpr, tpr, threshold):
 
   return threshold_index
 
+def analysis_ips(threshold, test_scores, 
+                 test_src_ips, test_dest_ips):
+  ip_counts = {}
+  for i in range(len(test_scores)):
+    if test_scores[i] > threshold:
+      src_ip = test_src_ips[i]
+      dest_ip = test_dest_ips[i]
+      if src_ip not in ip_counts:
+        ip_counts[src_ip] = 0
+      if dest_ip not in ip_counts:
+        ip_counts[dest_ip] = 0
+      ip_counts[src_ip] += 1
+      ip_counts[dest_ip] += 1
 
+  return ip_counts
 
 """
   Performs the analysis.
@@ -39,8 +52,18 @@ def find_optimal_cutoff_closest_to_perfect(fpr, tpr, threshold):
   test_X - The test data.
   plot - If true, plots the ROC and Precision/Recall curves.
   problem_name - Used to label the plot. 
+  test_src_ips - The source ips for the netflows of test set
+  test_dest_ips - The dest ips for the netflows of the test set
 """
-def analysis(figurenum, train_y, train_X, test_y, test_X, plot, problem_name):
+def analysis(figurenum, 
+             train_y, 
+             train_X, 
+             test_y, 
+             test_X, 
+             plot, 
+             problem_name,
+             test_src_ips,
+             test_dest_ips):
   clf = RandomForestClassifier()
   clf.fit(train_X, train_y)
 
@@ -50,8 +73,15 @@ def analysis(figurenum, train_y, train_X, test_y, test_X, plot, problem_name):
   test_auc = roc_auc_score(test_y, test_scores)
   train_average_precision = average_precision_score(train_y, train_scores)
   test_average_precision = average_precision_score(test_y, test_scores)
-  fpr, tpr, thresholds = roc_curve(test_y, clf.predict_proba(test_X)[:,1])
+  fpr, tpr, thresholds = roc_curve(test_y, test_scores)
   threshold_index = find_optimal_cutoff_closest_to_perfect(fpr, tpr, thresholds)
+
+  ip_counts = analysis_ips(thresholds[threshold_index], test_scores, 
+                           test_src_ips, test_dest_ips)
+  sorted_ip_counts = sorted(ip_counts.items(), key=operator.itemgetter(1),
+                            reverse=True)
+  print( sorted_ip_counts[0:10] )
+
   print( "AUC of ROC on train", train_auc )
   print( "AUC of ROC on test", test_auc )
   print( "Average Precision on train", train_average_precision )
@@ -92,21 +122,27 @@ def analysis(figurenum, train_y, train_X, test_y, test_X, plot, problem_name):
 def main():
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--inputfile', type=str,
-                      required=True,
+  parser.add_argument('--inputfile', type=str, required=True,
                       help="The file with the features and labels.")
   parser.add_argument('--problem_name', type=str,
                       default="SpecifyProblemName",
                       help="The problem name used in plots")
   parser.add_argument('--plot', action='store_true')
+  parser.add_argument('--src_ips', type=str, required=True,
+                      help="File with list of source ips")
+  parser.add_argument('--dest_ips', type=str, required=True,
+                      help="File with list of dest ips")
                       
   FLAGS = parser.parse_args()
+
+  srcIps = np.loadtxt(FLAGS.src_ips, dtype=np.str)
+  destIps = np.loadtxt(FLAGS.dest_ips, dtype=np.str)
 
   with open(FLAGS.inputfile, "r") as infile:
     data = np.loadtxt(infile, delimiter=",")
 
-    y = data[:, 0]
-    X = data[:, 1:] 
+    y = data[:, 0] #labels are in the first column
+    X = data[:, 1:] #features are in the rest of the columns
 
     numNonZero = np.count_nonzero(y)
     numFound = 0
@@ -125,8 +161,12 @@ def main():
     print( "Share X1, X2", X1.shape, X2.shape )
     print( "Nonzero y1, y2", np.count_nonzero(y1), np.count_nonzero(y2) )
     figurenum = 0
-    figurenum = analysis(figurenum, y1, X1, y2, X2, FLAGS.plot, FLAGS.problem_name)
-    figurenum = analysis(figurenum, y2, X2, y1, X1, FLAGS.plot, FLAGS.problem_name)
+    figurenum = analysis(figurenum, y1, X1, y2, X2, FLAGS.plot, 
+                          FLAGS.problem_name, srcIps[i:], destIps[i:])
+    figurenum = analysis(figurenum, y2, X2, y1, X1, FLAGS.plot, 
+                          FLAGS.problem_name, srcIps[0:i], destIps[0:i])
 
-    input()
+    if FLAGS.plot:
+      input()
+
 main()
