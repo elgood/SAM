f0feacdbe9859784eb9edde003d85e41e00fdfca
elgood <elgood@node0.sam64.streaminggraphs-pg0.wisc.cloudlab.us>
2018-07-25 22:38:49 -0500

    TemporalSet with some new tests.

--- SamSrc/TemporalSet.hpp
+++ SamSrc/TemporalSet.hpp
@@ -3,41 +3,33 @@
 
 #include <cstddef>
 #include <mutex>
-#include <set>
+#include <map>
+#include <list>
 #include "Util.hpp"
 
 namespace sam {
 
-namespace detail {
-
-template <typename PairType>
-class PairCompare
-{
-  bool operator()(PairType const& lhs, PairType const& rhs)
-  {
-    return lhs.first < rhs.first;
-  }
+class TemporalSetException : public std::runtime_error {
+public:
+  TemporalSetException(char const * message) : std::runtime_error(message) { } 
+  TemporalSetException(std::string message) : std::runtime_error(message) { } 
 };
-
-}
 
 /**
  * A thread safe set data structure where the elements can expire, i.e. 
  * they have a limited temporal existence.  
  */
-template <typename V, typename TimeType>
+template <typename K, typename TimeType>
 class TemporalSet
 {
 public:
-  typedef std::pair<V, TimeType> PairType;
-  typedef detail::PairCompare<PairType> Compare;
-  typedef std::set<PairType, Compare> SetType;
-  typedef std::map<V, TimeType> MapType;
+  typedef std::pair<K, TimeType> PairType;
+  typedef std::list<PairType> ListType;
+  typedef std::map<K, TimeType> MapType;
 
 private:
 
-
-  /// How long a value has to live after being inserted.
+  /// How long a key has to live after being inserted.
   TimeType timeToLive;
 
   // How many mutex slots in the table.
@@ -46,26 +38,43 @@ private:
   // Locks for each table element
   std::mutex* mutexes;
 
-  // An array (size tableCapacity) of sets 
-  MapType* sets; 
+  // An array (size tableCapacity) of maps.  The mapping is from keys to the
+  // time that the key occurred. 
+  MapType* hashTables; 
+
+  // This keeps the keys in temporal order so we can delete them from
+  // the hash table without iterating over the whole hash table.
+  ListType* lists;
 
   /// The hash function to use.
-  std::function<size_t(V const&)> hashFunction;
+  std::function<size_t(K const&)> hashFunction;
 
 public:
 
-  TemporalSet(size_t tableCapacity, 
-              std::function<size_t(V const&)> hashFunction)
+  TemporalSet()
   {
-    this->hashFunction = hashFunction;
-    mutexes = new std::mutex[tableCapacity];
-    sets = new MapType[tableCapacity]; 
+    tableCapacity = 0;
+    hashFunction = 0;
+    timeToLive = 0;
+  }
+
+  TemporalSet(size_t tableCapacity, 
+              std::function<size_t(K const&)> hashFunction,
+              TimeType timeToLive)
+  {
+    this->tableCapacity = tableCapacity;
+    this->hashFunction  = hashFunction;
+    this->timeToLive    = timeToLive;
+    mutexes    = new std::mutex[tableCapacity];
+    hashTables = new MapType[tableCapacity]; 
+    lists      = new ListType[tableCapacity];
   }
 
   ~TemporalSet()
   {
     delete[] mutexes;
-    delete[] sets;
+    delete[] lists;
+    delete[] hashTables;
   }
 
   /**
@@ -77,24 +86,85 @@ public:
    * such, besides inserting the value, we also remove any values from the 
    * set data structure that have expired.
    *
-   * \param value The value to insert into the set data structure.
+   * \param key The key to insert into the set data structure.
    * \param currentTime The time that the value occurred.
    * \return Returns true if the value was newly inserted, false if the
    *  value already existed and the time was updated.
    */
-  bool insert(V const& value, TimeType currentTime) 
+  bool insert(K const& key, TimeType currentTime) 
   {
-    size_t index = hashFunction(value) % tableCapacity;
-    mutexes[index].lock();
-    
 
-    sets[index].insert(pair);
+    PairType pair(key, currentTime);
+    size_t index = hashFunction(key) % tableCapacity;
+
+    // Lock out this hash table and list.
+    mutexes[index].lock();
+
+    MapType& map = hashTables[index];
+    ListType& list = lists[index];
+ 
+    double previousTime = 0;
+    if (list.size() > 0) {
+      previousTime = list.back().second;
+    }
+    if (currentTime < previousTime) {
+      throw TemporalSetException("TemporalSet::insert currentTime < "
+        "previousTime"); 
+    }
+
+    // First erase edges.  We look in the list first, which should be ordered
+    // by time, and then erase corresponding entries in the map.
+    auto iter = list.begin();
+    for (; iter != list.end();) 
+    {
+      TimeType t = iter->second;
+     
+      if (currentTime - t > timeToLive) {
+        
+        K deleteKey = iter->first;
+        map.erase(deleteKey);
+        iter = list.erase(iter);
+        
+      } else {
+        ++iter;
+      }
+    }
+
+    // Add the key/currentTime pair to the end of the list.
+    list.push_back(pair);
+
+    // Add the key and it's associated time to the hash table.
+    map[key] = currentTime;
     
-    // iterate from the beginning and delete
-    auto 
     mutexes[index].unlock();
+
+    return true;
   }
 
+  /**
+   * Returns true if the key is found in the set. 
+   */ 
+  bool contains(K key)
+  {
+    size_t index = hashFunction(key) % tableCapacity;
+    mutexes[index].lock();
+    bool result = hashTables[index].count(key) > 0;
+    mutexes[index].unlock();
+    return result; 
+  }
+
+  /**
+   * Returns the number keys in the set.
+   */
+  size_t size()
+  {
+    size_t total = 0;
+    for (size_t i = 0; i < tableSize; i++) 
+    {
+      total += hashTables[i].size() 
+    }
+    return total;
+  }
 };
   
 } //end namespace sam
--- TestSrc/TestTemporalSet.cpp
+++ TestSrc/TestTemporalSet.cpp
@@ -2,18 +2,59 @@
 
 #include "TemporalSet.hpp"
 #include <boost/test/unit_test.hpp>
+#include <thread>
 
 
 using namespace sam;
 
-BOOST_AUTO_TEST_CASE( test_temporal_set )
+typedef TemporalSet<size_t, double> SetType;
+
+struct F 
 {
-  typedef TemporalSet<size_t, double> SetType;
-
-  size_t tableSize = 1000;
-  
+  // Want all keys to go to same table entry.
+  size_t tableSize = 1;
+  double timeToLive = 20.0;
   UnsignedIntHashFunction hash; 
+  SetType* set;
 
-  SetType set(tableSize, hash);
+  F() {
+    set = new SetType(tableSize, hash, timeToLive);
+  }
+
+  ~F() {
+    delete set;
+  }
+};
+
+BOOST_FIXTURE_TEST_CASE( test_exception, F )
+{
+  set->insert(10, 10.0);
+  BOOST_CHECK_THROW( set->insert(10, 0.0), TemporalSetException );
 }
 
+BOOST_FIXTURE_TEST_CASE( test_contains, F )
+{
+  size_t key = 10;
+  set->insert(key, 0.0);
+  bool result = set->contains(key);
+  BOOST_CHECK_EQUAL(result, true);
+  result = set->contains(100);
+  BOOST_CHECK_EQUAL(result, false);
+}
+
+BOOST_FIXTURE_TEST_CASE( test_delete, F )
+{
+  size_t key1 = 10;
+  set->insert(key1, 0.0);
+  BOOST_CHECK(set->contains(key1));
+  size_t key2 = 20;
+  set->insert(key2, 50.0);
+  BOOST_CHECK(set->contains(key2));
+  BOOST_CHECK(!set->contains(key1));
+}
+
+BOOST_AUTO_TEST_CASE( test_multi_threads )
+{
+
+
+}
