d9d2bb5fb698c234cee46def0240019449584184
elgood <eric.goodman@colorado.edu>
2020-01-01 20:12:58 -0700

    Got rid of ip processing.

--- scripts/learning.py
+++ scripts/learning.py
@@ -24,46 +24,29 @@ def find_optimal_cutoff_closest_to_perfect(fpr, tpr, threshold):
 
   return threshold_index
 
-def analysis_ips(threshold, test_scores, 
-                 test_src_ips, test_dest_ips):
-  ip_counts = {}
-  for i in range(len(test_scores)):
-    if test_scores[i] > threshold:
-      src_ip = test_src_ips[i]
-      dest_ip = test_dest_ips[i]
-      if src_ip not in ip_counts:
-        ip_counts[src_ip] = 0
-      if dest_ip not in ip_counts:
-        ip_counts[dest_ip] = 0
-      ip_counts[src_ip] += 1
-      ip_counts[dest_ip] += 1
-
-  return ip_counts
-
-"""
-  Performs the analysis.
-  
-  Arguments:
-  figurenum - Used to increment the figurenum between calls so we don't plot
-              different data to the same figure.
-  train_y - The training labels.
-  train_X - The training data.
-  test_y - The test labels.
-  test_X - The test data.
-  plot - If true, plots the ROC and Precision/Recall curves.
-  problem_name - Used to label the plot. 
-  test_src_ips - The source ips for the netflows of test set
-  test_dest_ips - The dest ips for the netflows of the test set
-"""
 def analysis(figurenum, 
              train_y, 
              train_X, 
              test_y, 
              test_X, 
              plot, 
-             problem_name,
-             test_src_ips,
-             test_dest_ips):
+             problem_name):
+  """
+    Performs the analysis.
+    
+    Arguments:
+    figurenum - Used to increment the figurenum between calls so we don't plot
+                different data to the same figure.
+    train_y - The training labels.
+    train_X - The training data.
+    test_y - The test labels.
+    test_X - The test data.
+    plot - If true, plots the ROC and Precision/Recall curves.
+    problem_name - Used to label the plot. 
+    test_src_ips - The source ips for the netflows of test set
+    test_dest_ips - The dest ips for the netflows of the test set
+  """
+
   clf = RandomForestClassifier()
   clf.fit(train_X, train_y)
 
@@ -75,12 +58,6 @@ def analysis(figurenum,
   test_average_precision = average_precision_score(test_y, test_scores)
   fpr, tpr, thresholds = roc_curve(test_y, test_scores)
   threshold_index = find_optimal_cutoff_closest_to_perfect(fpr, tpr, thresholds)
-
-  ip_counts = analysis_ips(thresholds[threshold_index], test_scores, 
-                           test_src_ips, test_dest_ips)
-  sorted_ip_counts = sorted(ip_counts.items(), key=operator.itemgetter(1),
-                            reverse=True)
-  print( sorted_ip_counts[0:10] )
 
   print( "AUC of ROC on train", train_auc )
   print( "AUC of ROC on test", test_auc )
@@ -128,17 +105,10 @@ def main():
                       default="SpecifyProblemName",
                       help="The problem name used in plots")
   parser.add_argument('--plot', action='store_true')
-  parser.add_argument('--src_ips', type=str, required=True,
-                      help="File with list of source ips")
-  parser.add_argument('--dest_ips', type=str, required=True,
-                      help="File with list of dest ips")
   parser.add_argument('--subset', type=str,
     help="Comma-separated list of features to include") 
                       
   FLAGS = parser.parse_args()
-
-  srcIps = np.loadtxt(FLAGS.src_ips, dtype=np.str)
-  destIps = np.loadtxt(FLAGS.dest_ips, dtype=np.str)
 
   with open(FLAGS.inputfile, "r") as infile:
     data = np.loadtxt(infile, delimiter=",")
@@ -170,9 +140,9 @@ def main():
     print( "Nonzero y1, y2", np.count_nonzero(y1), np.count_nonzero(y2) )
     figurenum = 0
     figurenum = analysis(figurenum, y1, X1, y2, X2, FLAGS.plot, 
-                          FLAGS.problem_name, srcIps[i:], destIps[i:])
+                          FLAGS.problem_name)#, srcIps[i:], destIps[i:])
     figurenum = analysis(figurenum, y2, X2, y1, X1, FLAGS.plot, 
-                          FLAGS.problem_name, srcIps[0:i], destIps[0:i])
+                          FLAGS.problem_name)#, srcIps[0:i], destIps[0:i])
 
 
     if FLAGS.plot:
