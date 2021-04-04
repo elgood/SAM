5dbb4cf0ae9d3f8466b6c02c73966f191bcee10b
cabacco <coba6526@colorado.edu>
2021-04-03 21:02:43 -0600

    Adding initial versions of JaccardIndex.hpp and TestJaccardIndex.cpp

---
+++ SamSrc/sam/JaccardIndex.hpp
@@ -0,0 +1,203 @@
+#ifndef JACCARD_INDEX_HPP
+#define JACCARD_INDEX_HPP
+
+/**
+ * This is a simple implementation of Jaccard index that is not space
+ * efficient (i.e. O(N) where N is the size of the sliding window).
+ */
+
+#include <iostream>
+#include <map>
+#include <bits/stdc++.h>       // needed for sets
+
+#include <boost/lexical_cast.hpp>
+#include <sam/AbstractConsumer.hpp>
+#include <sam/BaseComputation.hpp>
+#include <sam/Features.hpp>
+#include <sam/Util.hpp>
+#include <sam/FeatureProducer.hpp>
+#include <sam/tuples/Edge.hpp>
+
+namespace sam
+{
+
+namespace JaccardIndexDetails {
+
+template <typename T>
+class JaccardIndexDataStructure {
+private:
+  size_t N;
+  T* array;
+  int current = 0;
+  // sets used for calculating the Jaccard index
+  std::set<T> setA;
+  std::set<T> setB;
+
+public:
+  JaccardIndexDataStructure(size_t N) {
+    // Allocate and initialize the data strcutures
+    this->N = N;
+    array = new T[N]();
+    for (int i = 0; i < N; i++) array[i] = 0;
+  }
+
+  ~JaccardIndexDataStructure() {
+    delete[] array;
+
+    // what clean up should be done for sets?????
+//    delete setA;
+//    delete setB;
+  }
+
+  /**
+   * Adds an item, removing an item if need be.
+   */
+  void insert(T item) {
+    array[current] = item;
+    std::cout << "array[" << current << "] = " << array[current] << std::endl; // debug
+    current++;
+    if (current >= N) {
+      current = 0;
+    }
+  }
+
+  T getJaccardIndex() {
+    /**
+     * Walk through array putting all values from first half into setA
+     * and putting the values from the second half into setB
+    */
+    for (int i = 0; i < N; i++) {
+      if (i <= ((N/2)-1)) {
+        setA.insert(array[i]);
+//        std::cout << "setA[" << i << "] = " << setA[i] << std::endl; // debug
+      }
+      else {
+        setB.insert(array[i]);
+//        std::cout << "setB[" << i << "] = " << setB[i] << std::endl; // debug
+      }
+    }
+    /**
+     * Let number of elements in set A & B be size_A & size_B respectively, and
+     * the number of elements in the intersection of sets A & B be size_intAB,
+     * then the Jaccard index can be computed by:
+     * size_intAB / (size_A + size_B - size_intAB)
+     *
+     * Ref: (https://en.wikipedia.org/wiki/Jaccard_index)
+     */
+    double size_A = setA.size();
+    double size_B = setB.size();
+    std::set<T> intersect;
+
+    // Find the intersection of the two sets
+    set_intersection(setA.begin(), setA.end(), setB.begin(), setB.end(),
+                     inserter(intersect, intersect.begin()));
+    double size_intAB = intersect.size();
+
+    // Compute the Jaccard index
+    double jaccardIndex = size_intAB / (size_A + size_B - size_intAB);
+
+    std::cout << "INDEX = " << jaccardIndex << std::endl; // debug
+
+    return jaccardIndex;
+  }
+};
+
+}
+
+
+template <typename T, typename EdgeType,
+          size_t valueField, size_t... keyFields>
+class JaccardIndex: public AbstractConsumer<EdgeType>,
+                    public BaseComputation,
+                    public FeatureProducer
+{
+public:
+  typedef typename EdgeType::LocalTupleType TupleType;
+private:
+  size_t N; ///> Size of sliding window
+  typedef JaccardIndexDetails::JaccardIndexDataStructure<T> value_t;
+
+  /// Mapping from the key (e.g. an ip field) to the jaccard index
+  /// data structure that is keeping track of the values seen.
+  std::map<std::string, value_t*> allWindows;
+
+  // Where the most recent item is located in the array.
+  size_t top = 0;
+
+public:
+  JaccardIndex(size_t N,
+               size_t nodeId,
+               std::shared_ptr<FeatureMap> featureMap,
+               std::string identifier) :
+    BaseComputation(nodeId, featureMap, identifier)
+  {
+    this->N = N;
+  }
+
+  ~JaccardIndex()   {
+    for (auto p : allWindows) {
+      delete p.second;
+    }
+  }
+
+  bool consume(EdgeType const& edge)
+  {
+    TupleType tuple = edge.tuple;
+
+    this->feedCount++;
+    if (this->feedCount % this->metricInterval == 0) {
+      std::cout << "JaccardIndex: NodeId " << this->nodeId << " feedCount "
+                << this->feedCount << std::endl;
+    }
+
+    // Generates unique key from key fields
+    std::string key = generateKey<keyFields...>(tuple);
+    std::cout << "key = " << key << std::endl; // debug
+    if (allWindows.count(key) == 0) {
+      auto value = new value_t(N);
+      allWindows[key] = value;
+    }
+
+    std::string sValue =
+      boost::lexical_cast<std::string>(std::get<valueField>(tuple));
+    T value;
+    try {
+      value = boost::lexical_cast<T>(sValue);
+    } catch (std::exception e) {
+      std::cerr << "JaccardIndex::consume Caught exception trying to cast string "
+                << "value of " << sValue << std::endl;
+      std::cerr << e.what() << std::endl;
+      value = 0;
+    }
+
+    allWindows[key]->insert(value);
+
+    // Getting the current Jaccard Index and providing that to the featureMap.
+    T currentJaccardIndex = allWindows[key]->getJaccardIndex();
+    SingleFeature feature(currentJaccardIndex);
+    this->featureMap->updateInsert(key, this->identifier, feature);
+
+    notifySubscribers(edge.id, currentJaccardIndex);
+
+    return true;
+  }
+
+  T getJaccardIndex(std::string key) {
+    return allWindows[key]->getJaccardIndex();
+  }
+
+  std::vector<std::string> keys() const {
+    std::vector<std::string> theKeys;
+    for (auto p : allWindows) {
+      theKeys.push_back(p.first);
+    }
+    return theKeys;
+  }
+
+  void terminate() {}
+};
+
+}
+
+
+#endif
---
+++ TestSrc/TestJaccardIndex.cpp
@@ -0,0 +1,66 @@
+#define BOOST_TEST_MAIN TestJaccardIndex
+#include <boost/test/unit_test.hpp>
+#include <sam/JaccardIndex.hpp>
+#include <sam/tuples/VastNetflow.hpp>
+#include <sam/tuples/Edge.hpp>
+#include <sam/tuples/Tuplizer.hpp>
+
+using namespace sam;
+using namespace sam::vast_netflow;
+using std::string;
+
+typedef VastNetflow TupleType;
+typedef EmptyLabel LabelType;
+typedef Edge<size_t, LabelType, TupleType> EdgeType;
+typedef TuplizerFunction<EdgeType, MakeVastNetflow> Tuplizer;
+
+/**
+ * This is a reworking of TestSimpleSum and the tests need to be redone to
+ * be appropriate for Jaccard Index.
+ */
+BOOST_AUTO_TEST_CASE( jaccard_index_test )
+{
+  Tuplizer tuplizer;
+
+  std::vector<size_t> keyFields;
+  size_t nodeId = 0;
+  auto featureMap = std::make_shared<FeatureMap>();
+  JaccardIndex<size_t, EdgeType, SrcTotalBytes, DestIp>
+    ji(10, nodeId, featureMap, "sum0");
+
+
+  string netflowString1 = "1365582756.384094,2013-04-10 08:32:36,"
+                         "20130410083236.384094,17,UDP,172.20.2.18,"
+                         "239.255.255.250,29986,1900,0,0,0,133,0,1,0,1,0,0";
+  string netflowString2 = "1365582756.384094,2013-04-10 08:32:36,"
+                         "20130410083236.384094,17,UDP,172.20.2.18,"
+                         "239.255.255.250,29986,1900,0,0,0,133,0,2,0,1,0,0";
+
+  EdgeType edge1 = tuplizer(1, netflowString1);
+  EdgeType edge2 = tuplizer(2, netflowString2);
+
+
+  ji.consume(edge1);
+  float jaccardindex = ji.getJaccardIndex("239.255.255.250");
+  BOOST_CHECK_EQUAL(jaccardindex, 0.5);
+
+  for (int i = 0; i < 9; i++) ji.consume(edge1);
+  jaccardindex = ji.getJaccardIndex("239.255.255.250");
+  BOOST_CHECK_EQUAL(jaccardindex, 10);
+
+
+  for (int i = 0; i < 9; i++)
+  {
+    ji.consume(edge1);
+    jaccardindex = ji.getJaccardIndex("239.255.255.250");
+    BOOST_CHECK_EQUAL(jaccardindex, 10);
+  }
+
+  ji.consume(edge2);
+  jaccardindex = ji.getJaccardIndex("239.255.255.250");
+  BOOST_CHECK_EQUAL(jaccardindex, 11);
+
+  ji.consume(edge2);
+  jaccardindex = ji.getJaccardIndex("239.255.255.250");
+  BOOST_CHECK_EQUAL(jaccardindex, 12);
+}
